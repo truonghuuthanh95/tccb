b38bfc4e14346a834b625d72294c84877cc621d0
truonghuuthanh <truonghuuthanh95@gmail.com>
2018-07-07 15:39:45 +0700

    fix nam vao nganh, he so luong, moc nang luong lan sau, ma nghach ko update dc

--- TCCB/TCCB/Utils/CommonConstants.cs
+++ TCCB/TCCB/Utils/CommonConstants.cs
@@ -8,8 +8,8 @@ namespace TCCB
     public static class CommonConstants
     {
         public static string USER_SESSION = "USER_SESSION";
-        public static string BASE_URL = "http://test.tuyendung.hcm.edu.vn";
-        //public static string BASE_URL = "http://localhost:60131/";
+        //public static string BASE_URL = "http://test.tuyendung.hcm.edu.vn";
+        public static string BASE_URL = "http://localhost:60131/";
         public static string MANAGEMENTUNIT = "MANAGEMENTUNIT";
         public static string REGISTRATION_PRICE = "REGISTRATION_PRICE";
         public static string REGISTED_INTERVIEW = "REGISTED_INTERVIEW";
